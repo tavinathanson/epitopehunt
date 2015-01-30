5932c30dd267471d68ef55b5c9a54fbcd219d399
Tavi Nathanson <tavi.nathanson@gmail.com>
2015-01-30 14:13:19 -0500

    Really simple pepdata usage

--- epitopehunt/views.py
+++ epitopehunt/views.py
@@ -1,8 +1,26 @@
 from flask import Flask, render_template
+from pandas import concat
+from pepdata import iedb
 
 from epitopehunt import app
+
+
+EPITOPE_SUB_SEQ_LENGTH = 4
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/matches')
+def matches():
+    epitope = "AAAALLLL"
+    df = iedb.load_tcell()
+    matches_dfs = []
+    for i in range(len(epitope) - EPITOPE_SUB_SEQ_LENGTH + 1):
+        sub_seq = epitope[i:i + EPITOPE_SUB_SEQ_LENGTH]
+        matches = df[df['Epitope Linear Sequence'].str.contains(sub_seq)]
+        matches_dfs.append(matches)
+    return concat(matches_dfs).to_html()
+
